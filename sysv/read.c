230622688cdea15e3a1e21426673b356db6aa0b3
Jack Li <ljh_app@qq.com>
2021-09-20 01:02:29 +0800

    Update read.c

--- sysv/read.c
+++ sysv/read.c
@@ -1,56 +1,41 @@
 //read.c
-//ipcs
-//ipcrm sem 12
-//ipcrm shm 34
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/sem.h>
-#include <signal.h>
 #include "shm.h"
 
 void *g_shmptr = NULL;
-int g_shmid;
 int g_sem_r;
 int g_sem_w;
-struct sembuf g_semop;
 
-void handler(int sig)
-{
-    //don't remove it, just leave it there
-    //so, read, write, each side can quit or start at any time
-    // shmdt(g_shmptr);
-    // semctl(g_sem_r,0,IPC_RMID);
-    // semctl(g_sem_w,0,IPC_RMID);
-    // exit(0);
-}
-
+//don't remove sem, shm in the code, just leave them there.
+//so read, write, each side can quit or start at any time.
+//use ipcrm to remove them from command line.
 void init()
 {
-    // signal(SIGINT,handler); //don't remove it, just leave it there
-
-    g_shmid = shmget(SHM,LEN,IPC_CREAT|0666);
-    if(g_shmid==-1){
+    int shmid = shmget(SHM, LEN, IPC_CREAT | 0666);
+    if(shmid == -1){
         perror("shmget");
         exit(-1);
     }
 
-    g_shmptr = shmat(g_shmid,0,0);
-    if(g_shmptr==(void *)-1){
+    g_shmptr = shmat(shmid, 0, 0);
+    if(g_shmptr == (void *)-1){
         perror("shmat");
         exit(-1);
     }
 
-    g_sem_r = semget(SEM1,1,IPC_CREAT|0666);
+    g_sem_r = semget(SEM_R,1,IPC_CREAT|0666);
     if(g_sem_r==-1){
         perror("semget");
         exit(-1);
     }
 
-    g_sem_w = semget(SEM2,1,IPC_CREAT|0666);
+    g_sem_w = semget(SEM_W,1,IPC_CREAT|0666);
     if(g_sem_w==-1){
         perror("semget");
         exit(-1);
@@ -63,11 +48,11 @@ int main()
 
     while(1){
         printf("%s: %s\n", __FILE__, "P");
-        P(g_sem_w, g_semop);
+        P(g_sem_w);
 
-        // printf("%s: %s\n", __FILE__, (char *)g_shmptr);
+        printf("%s: %s\n", __FILE__, (char *)g_shmptr);
 
-        V(g_sem_r, g_semop);
+        V(g_sem_r);
         printf("%s: %s\n", __FILE__, "V");
     }
 
