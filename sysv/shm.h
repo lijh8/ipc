a01df939dcfbaf8f2ebe607dd259752e323fcc17
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-07-08 16:13:30 +0800

    Update shm.h

--- sysv/shm.h
+++ sysv/shm.h
@@ -14,7 +14,29 @@
 
 //P: 🅿️ in Car Park. Stop, Wait.
 //V: VP, UP, Wake UP. they look similar
-#define P(sem) semop(sem, &(struct sembuf){.sem_num = 0, .sem_op = -1, .sem_flg = 0}, 1)
-#define V(sem) semop(sem, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 0}, 1)
+
+// error: Taking the address of a temporary object of type 'T'
+
+// #define P(sem) semop(sem, &(struct sembuf){.sem_num = 0, .sem_op = -1, .sem_flg = 0}, 1)
+// #define V(sem) semop(sem, &(struct sembuf){.sem_num = 0, .sem_op = 1, .sem_flg = 0}, 1)
+
+#define P(semid) do { \
+    struct sembuf sops = {.sem_num = 0, .sem_op = -1, .sem_flg = 0}; \
+    errno = 0; \
+    auto n = semop(semid, &sops, 1); \
+    if (n != 0){ \
+        perror("P semop"); \
+    } \
+} while (0)
+
+#define V(semid) do { \
+    struct sembuf sops = {.sem_num = 0, .sem_op = 1, .sem_flg = 0}; \
+    errno = 0; \
+    auto n = semop(semid, &sops, 1); \
+    if (n != 0){ \
+       perror("V semop"); \
+    } \
+} while (0)
+
 
 #endif
